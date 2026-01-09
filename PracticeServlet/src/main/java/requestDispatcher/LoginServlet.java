a740bea433b2f67b8c735bafed4208340c30f97c
Om Mahajan <ommahajan385@gmail.com>
2026-01-09 14:01:58 +0530

    login form added

---
+++ PracticeServlet/src/main/java/requestDispatcher/LoginServlet.java
@@ -0,0 +1,30 @@
+package requestDispatcher;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+
+		PrintWriter pw = resp.getWriter();
+		if (username.equals("admin") && password.equals("admin@123")) {
+			pw.print("<h1>Login Successfully !!!</h1>");
+		} else {
+			pw.print("<h1>Login Failed !!!</h1>" + "<h1>Try Again</h1>");
+			RequestDispatcher dispatcher = req.getRequestDispatcher("login.html");
+			dispatcher.include(req, resp);
+		}
+	}
+}
